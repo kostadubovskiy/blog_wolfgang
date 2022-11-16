f45d4731bf504dccdd18004219430186c2aab850
kostadubovskiy <kostadubovskiy@gmail.com>
2022-11-15 22:03:12 -0500

    final app—working, all important things have been fixed

--- app/__init__.py
+++ app/__init__.py
@@ -57,31 +57,52 @@ def home():
         if id[1] == curr_usr:
             my_blogs.append(id)
 
-    blogs = [0,0,0,0,0]
-    if len(my_blogs) < 5:
-        return render_template('home.html',text="Not enough blogs published", blog1_title="Publish at least 5 blogs!", \
-            blog2_title="Publish at least 5 blogs!", blog3_title="Publish at least 5 blogs!", blog4_title="Publish at least 5 blogs!", \
-                blog5_title="Publish at least 5 blogs!",)
-    if len(my_blogs) >= 5:
-        for i in range(5):
-            new_blog = list(random.choice(my_blogs))
-            while new_blog in blogs:
-                new_blog = list(random.choice(my_blogs))
-            blogs[i] = new_blog
-            blogs[i][0] = blogs[i][0]
+    # blogs = [0,0,0,0,0]
+    # if len(my_blogs) == 0:
+    #     return render_template('home.html',text="no blogs published")
+    # if len(my_blogs) >= 5:
+    #     for i in range(5):
+    #         blogs[i] = list(random.choice(my_blogs))
+    #         print(blogs[i][0])
+    #         blogs[i][0] = blogs[i][0]
+
+    html_input = '''<!DOCTYPE html> <html> <head> <link rel="stylesheet" href="{{ url_for('static',filename='styles/style.css') }}"> </head> <style> { border: solid black; } #blog_button { color: transparent; font-size: 0%; color: none; } #blog_label { margin-left: 5%; font-size: large; } body { background-color: ivory; font-family: Helvetica, Arial, sans-serif; } h2 { font-size: 40px; color: lightsalmon; } h1 { font-size: 60px; color: coral; text-decoration: underline; text-decoration-color: black; } h3 { font-size: 30px; color: tomato; } a { color: palevioletred; text-decoration: none; } a.current-page { background-color: lightcyan; } a.moreinfo { float: right; } a:hover { color: hotpink; font-style: italic; text-decoration: underline; background-color: lightgoldenrodyellow; } nav { list-style-type: none; overflow: hidden; background-color: khaki; } nav a { border: groove lightsalmon; float: left; display: block; text-align: center; padding: 16px; } div.body { display: flex; } div.text { display: block; margin: 1%; } div.stat-text { max-width: 55%; display: block; margin-left: 3%; margin-top: 5%; float: right; } div.large-stat-text { max-width: 95%; display: block; margin: auto; } div.right-img { margin: auto; max-width: 50% } div.left-img { max-width: 70%; } div.next-page { margin: 5%; text-align: center; } div.stat-left { max-width: 50%; padding: 1%; float: left; } div.large-stat-left { display: block; padding: 1%; max-width: 100% } div.large-stat-left3 { display: block; padding: 1%; max-width: 120% } img.left { width: 100%; height: auto; } img { border: groove lightsalmon; } img.stat-left { max-width: 100%; padding: 12%; height: 70%; width: auto; } img.large-stat-left { width: 75%; display: block; margin-left: auto; margin-right: auto; } img.large-stat-left3 { width: 90%; display: block; margin-left: auto; margin-right: auto; } </style> <body> <form action="/"> <input type="submit" value="Back"/> </form> <div class="tabs"> <a href="home">Home</a> <a href="explore">Explore</a> <a href="create">Create</a> <!-- <a class="current-page" href="goodpets.html">Why They're Good Pets</a> <a href="moreinfo.html">Continue Learning about Frogs</a> --> </div> <h1>Home!</h1> <div> <form method="POST">'''
+    html_end = '''</form></div></body></html>'''
+
+    html_temp = '''
+<br>
+    <label id="blog_label">
+    {}
+        <button type="submit" name="blog_id" id="blog_button" placeholder= {} value={}></button><br/>
+    </label>
+<br>'''
+
+    for blog in my_blogs:
+        blog_title = blog[2]
+        blog_id = str(blog[0])
+        html_input += (html_temp.format(blog_title,blog_title,blog_id))
+    
+    # print(html_input+html_end)
+    '''<br>
+            <label id="blog_label">
+            {blog1_title}
+                <button type="submit" name="blog_id" id="blog_button" placeholder={{blog1_title}} value={{blog1}}></button><br/>
+            </label>
+        <br>
+        '''
 
     if request.method == 'POST':
-        print("hi")
-        print(request.form['blog_id'])
         session['viewing_blog_id'] = request.form['blog_id']
         return redirect(url_for('blog'))
 
-    print(blogs)
 
-    return render_template('home.html',\
-        blog1=blogs[0][0], blog2=blogs[1][0], blog3=blogs[2][0], blog4=blogs[3][0], blog5=blogs[4][0],\
-        blog1_title=blogs[0][2], blog2_title=blogs[1][2], blog3_title=blogs[2][2], blog4_title=blogs[3][2],blog5_title=blogs[4][2]\
-    )
+
+    # return render_template('home.html',\
+    #     blog1=blogs[0][0], blog2=blogs[1][0], blog3=blogs[2][0], blog4=blogs[3][0], blog5=blogs[4][0],\
+    #     blog1_title=blogs[0][2], blog2_title=blogs[1][2], blog3_title=blogs[2][2], blog4_title=blogs[3][2],blog5_title=blogs[4][2]\
+    # )
+    # return render_template('home.html',all_blogs=html_input)
+    return (html_input+html_end)
 
 @app.route('/blog', methods=['GET', 'POST'])
 def blog():
